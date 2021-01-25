d39c074b4f1efe44a1f85b09f643132340bfc79f
Muhammad Farooq <m.farooq.h7@gmail.com>
2021-01-24 17:53:30 -0800

    Updates

---
+++ precache-manifest.5c4dbf31b24440c84767c3f94fe02fc3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03175405fa5c3d0e9a042137f5b645c5",
+    "url": "/React-amazon/index.html"
+  },
+  {
+    "revision": "9a9428e08945eda866c9",
+    "url": "/React-amazon/static/css/main.2b08e443.chunk.css"
+  },
+  {
+    "revision": "2965ee616f7796740245",
+    "url": "/React-amazon/static/js/2.32e9e253.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-amazon/static/js/2.32e9e253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a9428e08945eda866c9",
+    "url": "/React-amazon/static/js/main.a62ebfe0.chunk.js"
+  },
+  {
+    "revision": "e75ccd4cc5a9cbfd8673",
+    "url": "/React-amazon/static/js/runtime-main.ca2b1481.js"
+  },
+  {
+    "revision": "9174c19a7ad9866af5d61846f5919f4a",
+    "url": "/React-amazon/static/media/banner.9174c19a.png"
+  },
+  {
+    "revision": "15b89b283211d9ed5459bd532b625c0e",
+    "url": "/React-amazon/static/media/credit.15b89b28.png"
+  }
+]);
